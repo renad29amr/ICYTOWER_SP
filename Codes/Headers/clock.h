a576e4a4038ca0f851c7108f6273549058abb11e
Renad Amr Mohamed <renadamr.bls@gmail.com>
2025-06-23 19:13:55 +0300

    clock.h

---
+++ Codes/Headers/clock.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace sf;
+
+Clock transitionClock;
+static Clock FillTime;
+float clockFill = 0.0f;
+void clocktimer(float deltatime)
+{
+    static const float decayDuration = 10.0f;
+
+    if (clockFill > 0.0f)
+    {
+        float elapsed = FillTime.getElapsedTime().asSeconds();
+        if (elapsed <= decayDuration)
+        {
+            clockFill = 1.0f - (elapsed / decayDuration);
+        }
+
+        else
+        {
+            clockFill = 0.0f;
+        }
+    }
+}
